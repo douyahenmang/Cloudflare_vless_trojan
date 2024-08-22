932d0451f1fe69a88b2bf5777ea857bac8017429
douya <273344121@qq.com>
2024-08-22 14:21:21 +0800

    修改了uuid

--- Vless_workers_pages/_worker.js
+++ Vless_workers_pages/_worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "65a571fb-4fd2-4b37-8596-3c7d9728dd5c";
+let userID = "f265ba5d-5bfc-45ec-9677-342e33b684f9";
 
 const proxyIPs = ["proxy.xxxxxxxx.tk"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
 const cn_hostnames = [''];
